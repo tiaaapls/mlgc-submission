daeb3aa5bbe2ea99f1eedd508a0ee52429bdec30
Tia Pelisa <tiapelisa53@gmail.com>
2024-12-03 13:01:03 +0700

    first commit

---
+++ src/server/handler.js
@@ -0,0 +1,76 @@
+const crypto = require('crypto');
+const { Firestore } = require('@google-cloud/firestore');
+const predictClassification = require('../services/inferenceService');
+const storeData = require('../services/storeData');
+
+async function postPredictHandler(request, h) {
+    const { image } = request.payload;
+
+    if (Buffer.byteLength(image) > 1000000) {  
+        return h.response({
+            status: 'fail',
+            message: 'Payload content length greater than maximum allowed: 1000000',
+        }).code(413);
+    }
+
+    const { model } = request.server.app;
+
+    try {
+        const { label, suggestion } = await predictClassification(model, image);
+        const id = crypto.randomUUID();
+        const createdAt = new Date().toISOString();
+
+        const data = {
+            "id": id,
+            "result": label,
+            "suggestion": suggestion,
+            "createdAt": createdAt
+        };
+        
+        try {
+            await storeData(id, data);
+        } catch (error) {
+            return h.response({
+                status: 'fail',
+                message: 'Terjadi kesalahan saat menyimpan data prediksi',
+            }).code(500);
+        }
+
+        return h.response({
+            status: 'success',
+            message: 'Model is predicted successfully',
+            data
+        }).code(201);
+
+    } catch (error) {  
+        return h.response({
+            status: 'fail',
+            message: 'Terjadi kesalahan dalam melakukan prediksi',
+        }).code(400);
+    }
+}
+
+async function getHistoriesHandler(request, h) {
+    const db = new Firestore();
+    const predictCollection = db.collection('prediction');
+  
+    try {
+        const snapshot = await predictCollection.get();
+        const histories = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            history: doc.data(),
+        }));
+    
+        return h.response({
+            status: 'success',
+            data: histories,
+        });
+    } catch (error) {
+        return h.response({
+            status: 'fail',
+            message: 'Gagal mengambil data riwayat prediksi.',
+        }).code(500);
+    }
+}
+   
+module.exports = { postPredictHandler, getHistoriesHandler };
---
+++ src/services/inferenceService.js
@@ -0,0 +1,32 @@
+const tf = require('@tensorflow/tfjs-node');
+const InputError = require('../exceptions/InputError');
+ 
+async function predictClassification(model, image) {
+    try {
+        const tensor = tf.node
+            .decodeJpeg(image)
+            .resizeNearestNeighbor([224, 224])
+            .expandDims()
+            .toFloat();
+
+        const prediction = model.predict(tensor);
+        const scores = await prediction.data(); 
+        const confidenceScore = scores[0] * 100; 
+
+        let label, suggestion;
+
+        if (confidenceScore > 50) {
+            label = 'Cancer';
+            suggestion = "Segera periksa ke dokter!";
+        } else {
+            label = 'Non-cancer';
+            suggestion = "Penyakit kanker tidak terdeteksi.";
+        }
+
+        return { label, confidenceScore, suggestion };
+    } catch (error) {
+        throw new InputError(`Terjadi kesalahan dalam melakukan prediksi`);
+    }
+}
+
+module.exports = predictClassification;
